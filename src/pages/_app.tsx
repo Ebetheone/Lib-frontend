26d58b1fe0ba75578af534d753b68ed085816afa
Elbeg Darkhanbaatar <tab.dev.elbeg@gmail.com>
2023-12-09 03:40:52 +0800

    feat: fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,18 @@ import type { AppProps } from "next/app"
 import { ApolloProvider } from "@apollo/client"
 import "../../styles/globals.css"
 import { useApollo } from "src/lib/apollo/client"
+import { AuthProvider } from "src/context/AuthContext"
+import { useState } from "react"
+import { UserContextType } from "src/context/types"
 
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialState)
+  const [user, setUser] = useState<UserContextType | null>(null)
   return (
     <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
+      <AuthProvider user={user} setUser={setUser}>
+        <Component {...pageProps} />
+      </AuthProvider>
     </ApolloProvider>
   )
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -44,7 +44,6 @@ const Home = () => {
   })
 
   const { user } = useAuth()
-  console.log("user", user)
 
   const childrens: Record<ChildrensEnum, React.JSX.Element> = {
     [ChildrensEnum.BookDetail]: (
