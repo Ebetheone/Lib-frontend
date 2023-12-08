eed1167c9cfc113709314f53d6068c49498660ba
sapnai <sarnaii0207@gmail.com>
2023-12-08 21:35:17 +0800

    feat: Tabbar buttons

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,10 @@ import TabBar from "src/components/TabBar"
 const Home = () => {
   return (
     <div className="flex w-screen h-screen bg-[#E8E8E8] ">
-      <TabBar />
+      <TabBar
+        onClickBook={() => console.log("book")}
+        onClickProfile={() => console.log("Profile")}
+      />
       <div className="flex w-1/2 h-1/2 bg-black">hahaT</div>
     </div>
   )
