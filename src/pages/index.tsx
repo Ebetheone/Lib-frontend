8747cde5f17bf5bdd6cd57447cca45815bb29369
Elbeg Darkhanbaatar <tab.dev.elbeg@gmail.com>
2023-12-08 19:59:28 +0800

    feat: fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
 import React from "react"
+import { Button, Checkbox } from "antd"
 
 const Login = () => {
-  return <div>Login</div>
+  return (
+    <div className="w-100 flex items-center justify-center">
+      <div className="mt-10">Нэвтрэх</div>
+      <div>И-мэйл</div>
+      <div>
+        <input />
+      </div>
+      <div>Нууц үг</div>
+      <div>
+        <input />
+      </div>
+      <Checkbox>Нууц үг санах</Checkbox>
+      <Button name="Нэвтрэх" color="black" />
+      <Button name="Бүртгүүлэх" color="black" />
+    </div>
+  )
 }
 
 export default Login
